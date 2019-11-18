53534cfb8aad04d6de18433fdad71468dbd689b9
Specter <1292675809@qq.com>
2019-11-18 11:29:40 +0800

    Initial commit

---
+++ src/wormday/springmvc/helloworld/HiController.java
@@ -0,0 +1,15 @@
+package wormday.springmvc.helloworld;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/hi")
+public class HiController {
+    @RequestMapping("/say")
+    public String say(Model model){
+        model.addAttribute("name","wormday");
+        model.addAttribute("url","http://www.cnblogs.com/wormday/p/8435617.html");
+        return "say";
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Specter
+  Date: 2019/11/18
+  Time: 10:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>登录首页</title>
+  </head>
+  <body>
+  <div class="column">
+    <h2 class="ui image header" style="color: black;text-align: center">
+      用户登录
+    </h2>
+  </div>
+  <div>
+
+  </div>
+  </body>
+</html>
